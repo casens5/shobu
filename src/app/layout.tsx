eb7db4af632522502e739b2680f0784751d53dda
casens5 <rudolfordoyne@protonmail.com>
2024-11-26 16:19:03 -0600

    changes that probably don't matter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body >
+      <body className="min-h-screen p-0">
         {children}
       </body>
     </html>
