d59c17e69f3b41a31ecb38a86b8e2578049510ac
casens5 <rudolfordoyne@protonmail.com>
2024-12-03 10:38:46 -0600

    create separate backgrounds for home fields

--- src/app/game.tsx
+++ src/app/game.tsx
@@ -53,35 +53,39 @@ export default function Game() {
       <div className="text-right" onClick={() => clearAllPlayerMoves("black")}>
         clear black moves
       </div>
-      <div className="max-h-2xl grid h-auto w-full max-w-2xl grid-cols-2 items-center gap-x-7 gap-y-12 sm:gap-x-8 sm:gap-y-16">
-        <Board
-          ref={boardRefs[0]}
-          boardColor="dark"
-          playerTurn={playerTurn}
-          playerHome="white"
-          canPlay={true}
-        />
-        <Board
-          ref={boardRefs[1]}
-          boardColor="light"
-          playerTurn={playerTurn}
-          playerHome="white"
-          canPlay={true}
-        />
-        <Board
-          ref={boardRefs[2]}
-          boardColor="light"
-          playerTurn={playerTurn}
-          playerHome="black"
-          canPlay={true}
-        />
-        <Board
-          ref={boardRefs[3]}
-          boardColor="dark"
-          playerTurn={playerTurn}
-          playerHome="black"
-          canPlay={true}
-        />
+      <div className="max-h-2xl h-auto w-full max-w-2xl items-center">
+        <div className="grid grid-cols-2 gap-x-7 bg-[#00000088] py-6 sm:gap-x-8 sm:rounded-t-3xl sm:p-8">
+          <Board
+            ref={boardRefs[0]}
+            boardColor="dark"
+            playerTurn={playerTurn}
+            playerHome="white"
+            canPlay={true}
+          />
+          <Board
+            ref={boardRefs[1]}
+            boardColor="light"
+            playerTurn={playerTurn}
+            playerHome="white"
+            canPlay={true}
+          />
+        </div>
+        <div className="grid grid-cols-2 gap-x-7 bg-[#ffffff22] py-6 sm:gap-x-8 sm:rounded-b-3xl sm:p-8">
+          <Board
+            ref={boardRefs[2]}
+            boardColor="light"
+            playerTurn={playerTurn}
+            playerHome="black"
+            canPlay={true}
+          />
+          <Board
+            ref={boardRefs[3]}
+            boardColor="dark"
+            playerTurn={playerTurn}
+            playerHome="black"
+            canPlay={true}
+          />
+        </div>
       </div>
     </div>
   );
