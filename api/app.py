c1dead56cadfe163fc7d8797d56bf70c50c0acd0
casens5 <rudolfordoyne@protonmail.com>
2024-12-20 13:30:10 -0600

    test python configuration to fix vercel deployment

--- api/app.py
+++ api/app.py
@@ -1,6 +1,9 @@
 from flask import Flask 
 app = Flask(__name__)
 
-@app.route('/api/baba')
+@app.route('/api/baba', methods=['GET'])
 def baba():
     return "<p>baba!</p>"
+
+if __name__ == '__main__':
+    app.run(port=5328)
