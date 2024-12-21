0df5e9507cb8b8727463ac25d0e207cc063ded32
casens5 <rudolfordoyne@protonmail.com>
2024-12-20 20:15:46 -0600

    add concurrently

--- api/app.py
+++ api/app.py
@@ -1,9 +1,7 @@
 from flask import Flask 
+
 app = Flask(__name__)
 
 @app.route('/api/baba', methods=['GET'])
 def baba():
     return "<p>baba!</p>"
-
-if __name__ == '__main__':
-    app.run(port=5328)
