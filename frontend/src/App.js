aa3266d6379699b2bfce41415b141bb6e341e72b
casens5 <rudolfordoyne@protonmail.com>
2025-04-04 18:46:44 -0500

    [skip-ci]

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import Game from "./game";
+
+export default function App() {
+  return (
+    <div className="flex h-full min-h-screen flex-col items-center justify-center sm:p-8 bg-zinc-800 text-zinc-50">
+      <main className="flex h-full w-full flex-1 flex-col items-center text-xl">
+        <Game />
+      </main>
+      <footer className=""></footer>
+    </div>
+  );
+}
+
