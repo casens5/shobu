f7d706ce9cad3e7ed154da4801d45a9bc2608de2
casens5 <rudolfordoyne@protonmail.com>
2025-05-06 12:42:27 -0500

    make gameEngine helpers exportable/testable

--- frontend/src/game/gameEngine.tsx
+++ frontend/src/game/gameEngine.tsx
@@ -10,149 +10,146 @@ import {
   BoardType,
 } from "../types";
 
-//@ts-ignore
-export default function GameEngine(gameState, action) {
-  function getMoveLength(a: BoardCoordinates, b: BoardCoordinates): number {
-    return Math.max(Math.abs(a[0] - b[0]), Math.abs(a[1] - b[1]));
+export function getMoveLength(
+  a: BoardCoordinates,
+  b: BoardCoordinates,
+): number {
+  return Math.max(Math.abs(a[0] - b[0]), Math.abs(a[1] - b[1]));
+}
+
+export function switchPlayer(player: PlayerColor) {
+  return player === "white" ? "black" : "white";
+}
+
+export function getMoveDirection(
+  origin: BoardCoordinates,
+  destination: BoardCoordinates,
+): Direction {
+  // north/south movement
+  if (origin[0] === destination[0]) {
+    if (origin[1] > destination[1]) {
+      return Direction.N;
+    } else {
+      return Direction.S;
+    }
+  }
+  // east/west movement
+  if (origin[1] === destination[1]) {
+    if (origin[0] < destination[0]) {
+      return Direction.E;
+    } else {
+      return Direction.W;
+    }
+  }
+  // diagonal
+  if (origin[0] < destination[0] && origin[1] > destination[1]) {
+    return Direction.NE;
+  } else if (origin[0] < destination[0] && origin[1] < destination[1]) {
+    return Direction.SE;
+  } else if (origin[0] > destination[0] && origin[1] > destination[1]) {
+    return Direction.NW;
+  } else if (origin[0] > destination[0] && origin[1] < destination[1]) {
+    return Direction.SW;
   }
 
-  function switchPlayer(player: PlayerColor) {
-    return player === "white" ? "black" : "white";
+  console.error(`invalid direction: ${origin}, ${destination}`);
+  return Direction.N;
+}
+
+// checks that a move is legal locally on board, including direction, length, and pushing stones
+function isMoveLegal(
+  origin: BoardCoordinates,
+  destination: BoardCoordinates,
+  boardGrid: GridType,
+  playerTurn: PlayerColor,
+): BoardMessage | "LEGAL" {
+  const length = getMoveLength(origin, destination);
+
+  if ([0, 1, 2, 3].includes(length) !== true) {
+    console.error(`move length is some kind of crazy value.  value: ${length}`);
+    return BoardMessage.MOVEILLEGAL;
   }
-
-  function getMoveDirection(
-    origin: BoardCoordinates,
-    destination: BoardCoordinates,
-  ): Direction {
-    // north/south movement
-    if (origin[0] === destination[0]) {
-      if (origin[1] > destination[1]) {
-        return Direction.N;
-      } else {
-        return Direction.S;
-      }
-    }
-    // east/west movement
-    if (origin[1] === destination[1]) {
-      if (origin[0] < destination[0]) {
-        return Direction.E;
-      } else {
-        return Direction.W;
-      }
-    }
-    // diagonal
-    if (origin[0] < destination[0] && origin[1] > destination[1]) {
-      return Direction.NE;
-    } else if (origin[0] < destination[0] && origin[1] < destination[1]) {
-      return Direction.SE;
-    } else if (origin[0] > destination[0] && origin[1] > destination[1]) {
-      return Direction.NW;
-    } else if (origin[0] > destination[0] && origin[1] < destination[1]) {
-      return Direction.SW;
-    }
-
-    console.error(`invalid direction: ${origin}, ${destination}`);
-    return Direction.N;
-  }
-
-  // checks that a move is legal locally on board, including direction, length, and pushing stones
-  function isMoveLegal(
-    origin: BoardCoordinates,
-    destination: BoardCoordinates,
-    boardGrid: GridType,
-    playerTurn: PlayerColor,
-  ): BoardMessage | "LEGAL" {
-    const length = getMoveLength(origin, destination);
-
-    if ([0, 1, 2, 3].includes(length) !== true) {
-      console.error(
-        `move length is some kind of crazy value.  value: ${length}`,
-      );
-      return BoardMessage.MOVEILLEGAL;
-    }
-    // player selected and de-selected the stone
-    if (length === 0) {
-      return "LEGAL";
-    }
-    if (length === 3) {
-      return BoardMessage.MOVETOOLONG;
-    }
-
-    const moveLength = length as Length;
-    const direction = getMoveDirection(origin, destination);
-    direction;
-
-    // only allow orthogonal or diagonal moves, no knight moves
-    const xMove = Math.abs(origin[0] - destination[0]);
-    const yMove = Math.abs(origin[1] - destination[1]);
-    if (
-      !(xMove === 0 || xMove === moveLength) ||
-      !(yMove === 0 || yMove === moveLength)
-    ) {
-      return BoardMessage.MOVEKNIGHT;
-    }
-
-    const betweenCoords =
-      moveLength === 2
-        ? ([
-            origin[0] + (destination[0] - origin[0]) / 2,
-            origin[1] + (destination[1] - origin[1]) / 2,
-          ] as BoardCoordinates)
-        : undefined;
-    const nextX = destination[0] + (destination[0] - origin[0]) / moveLength;
-    const nextY = destination[1] + (destination[1] - origin[1]) / moveLength;
-    const nextCoords =
-      nextX >= 0 && nextX <= 3 && nextY >= 0 && nextY <= 3
-        ? ([nextX, nextY] as BoardCoordinates)
-        : undefined;
-
-    const destinationSquare = boardGrid[destination[0]][destination[1]];
-    const pushDestination = nextCoords
-      ? boardGrid[nextCoords[0]][nextCoords[1]]
-      : null;
-    const intermediarySquare = betweenCoords
-      ? boardGrid[betweenCoords[0]][betweenCoords[1]]
-      : null;
-
-    // can't push your own stone(s)
-    if (
-      (destinationSquare != null && destinationSquare.color === playerTurn) ||
-      (intermediarySquare != null && intermediarySquare.color === playerTurn)
-    ) {
-      return BoardMessage.MOVESAMECOLORBLOCKING;
-    }
-
-    // can't push 2 stones in a row
-    if (
-      Number(intermediarySquare != null) +
-        Number(destinationSquare != null) +
-        Number(pushDestination != null) >
-      1
-    ) {
-      return BoardMessage.MOVETWOSTONESBLOCKING;
-    }
-
+  // player selected and de-selected the stone
+  if (length === 0) {
     return "LEGAL";
   }
-
-  function checkWin(boardGrid: GridType): PlayerColor | null {
-    if (
-      !boardGrid.some((row) =>
-        row.some((cell) => cell && cell.color === "black"),
-      )
-    ) {
-      return "white";
-    }
-    if (
-      !boardGrid.some((row) =>
-        row.some((cell) => cell && cell.color === "white"),
-      )
-    ) {
-      return "black";
-    }
-    return null;
+  if (length === 3) {
+    return BoardMessage.MOVETOOLONG;
   }
 
+  const moveLength = length as Length;
+  const direction = getMoveDirection(origin, destination);
+  direction;
+
+  // only allow orthogonal or diagonal moves, no knight moves
+  const xMove = Math.abs(origin[0] - destination[0]);
+  const yMove = Math.abs(origin[1] - destination[1]);
+  if (
+    !(xMove === 0 || xMove === moveLength) ||
+    !(yMove === 0 || yMove === moveLength)
+  ) {
+    return BoardMessage.MOVEKNIGHT;
+  }
+
+  const betweenCoords =
+    moveLength === 2
+      ? ([
+          origin[0] + (destination[0] - origin[0]) / 2,
+          origin[1] + (destination[1] - origin[1]) / 2,
+        ] as BoardCoordinates)
+      : undefined;
+  const nextX = destination[0] + (destination[0] - origin[0]) / moveLength;
+  const nextY = destination[1] + (destination[1] - origin[1]) / moveLength;
+  const nextCoords =
+    nextX >= 0 && nextX <= 3 && nextY >= 0 && nextY <= 3
+      ? ([nextX, nextY] as BoardCoordinates)
+      : undefined;
+
+  const destinationSquare = boardGrid[destination[0]][destination[1]];
+  const pushDestination = nextCoords
+    ? boardGrid[nextCoords[0]][nextCoords[1]]
+    : null;
+  const intermediarySquare = betweenCoords
+    ? boardGrid[betweenCoords[0]][betweenCoords[1]]
+    : null;
+
+  // can't push your own stone(s)
+  if (
+    (destinationSquare != null && destinationSquare.color === playerTurn) ||
+    (intermediarySquare != null && intermediarySquare.color === playerTurn)
+  ) {
+    return BoardMessage.MOVESAMECOLORBLOCKING;
+  }
+
+  // can't push 2 stones in a row
+  if (
+    Number(intermediarySquare != null) +
+      Number(destinationSquare != null) +
+      Number(pushDestination != null) >
+    1
+  ) {
+    return BoardMessage.MOVETWOSTONESBLOCKING;
+  }
+
+  return "LEGAL";
+}
+
+function checkWin(boardGrid: GridType): PlayerColor | null {
+  if (
+    !boardGrid.some((row) => row.some((cell) => cell && cell.color === "black"))
+  ) {
+    return "white";
+  }
+  if (
+    !boardGrid.some((row) => row.some((cell) => cell && cell.color === "white"))
+  ) {
+    return "black";
+  }
+  return null;
+}
+
+//@ts-ignore
+export default function GameEngine(gameState, action) {
   // default to clearing the boardMessage
   const newGameState = { ...gameState, boardMessage: null };
   const newMoves = gameState.moves.slice();
